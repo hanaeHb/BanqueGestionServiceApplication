cfcabd87be939dbe973b2baea9ae809f8b2a34d1
hanae <hanaebertili@gmail.com>
2025-10-25 21:21:53 +0100

    add swagger

--- src/main/java/com/example/banquegestionservice/web/ApiRestfull.java
+++ src/main/java/com/example/banquegestionservice/web/ApiRestfull.java
@@ -3,10 +3,31 @@ package com.example.banquegestionservice.web;
 import com.example.banquegestionservice.dto.RequestCompteDto;
 import com.example.banquegestionservice.dto.ResponseCompteDto;
 import com.example.banquegestionservice.service.CompteServiceImpl;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Gestion des comptes bancaire",
+                description = "cette offre tous les méthodes pour gérer les comptes",
+                version = "1.0.0"
+        ),
+        servers = @Server(
+                url = "http://localhost:8081"
+        )
+)
+
 
 @RestController
 @RequestMapping("/v1/comptes")
@@ -16,12 +37,51 @@ public class ApiRestfull {
         this.compteService = compteService;
     }
 
+
+
+    @Operation(
+            summary = " Ajouter un compte",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = RequestCompteDto.class )
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "bien enregiter",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseCompteDto.class )
+                            )
+                    ),
+
+                    @ApiResponse(responseCode = "4xx",description = "erreur client"),
+                    @ApiResponse(responseCode = "5xx",description = "erreur serveur"),
+            }
+    )
+
     @PostMapping
     public ResponseEntity<ResponseCompteDto> add(@RequestBody RequestCompteDto requestCompteDto) {
        ResponseCompteDto responseCompteDto = compteService.Add_Compte(requestCompteDto);
        return ResponseEntity.ok(responseCompteDto);
 
     }
+
+    @Operation(
+            summary = " récuperer liste des compte",
+
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "bien enregiter",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = ResponseCompteDto.class ))
+                            )
+                    ),
+                    @ApiResponse(responseCode = "4xx",description = "erreur client"),
+                    @ApiResponse(responseCode = "5xx",description = "erreur serveur"),
+            }
+    )
 
     @GetMapping
     public ResponseEntity<List<ResponseCompteDto>> getAll() {
@@ -30,11 +90,49 @@ public class ApiRestfull {
         return ResponseEntity.ok(compteDtos);
     }
 
+    @Operation(
+            summary = " récupérer compte par Id",
+            parameters = @Parameter(name = "id", required = true),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "bien récuperer",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseCompteDto.class )
+                            )
+                    ),
+                    @ApiResponse(responseCode = "4xx",description = "erreur client"),
+                    @ApiResponse(responseCode = "5xx",description = "erreur serveur"),
+            }
+    )
+
     @GetMapping("/{id}")
     public ResponseEntity<ResponseCompteDto> getCompteById(@PathVariable Integer id) {
       ResponseCompteDto responseCompteDto = compteService.GETCompteById(id);
       return ResponseEntity.ok(responseCompteDto);
     }
+
+    @Operation(
+            summary = " Modifier un compte",
+            parameters = @Parameter(name = "id", required = true),
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = RequestCompteDto.class )
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "bien modifier",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseCompteDto.class )
+                            )
+                    ),
+
+                    @ApiResponse(responseCode = "4xx",description = "erreur client"),
+                    @ApiResponse(responseCode = "5xx",description = "erreur serveur"),
+            }
+    )
 
     @PutMapping("/{id}")
     public ResponseEntity<ResponseCompteDto> update(@PathVariable Integer id,
@@ -43,17 +141,63 @@ public class ApiRestfull {
         return ResponseEntity.ok(responseCompteDto);
     }
 
+    @Operation(
+            summary = " supprimer compte par Id",
+            parameters = @Parameter(name = "id", required = true),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "bien supprimer"),
+                    @ApiResponse(responseCode = "4xx",description = "erreur client"),
+                    @ApiResponse(responseCode = "5xx",description = "erreur serveur"),
+            }
+    )
+
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable Integer id) {
         compteService.DELETECompteBYID(id);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(
+            summary = " cette methode pour créditer un compte",
+            parameters = {
+                    @Parameter(name = "id",required = true),
+                    @Parameter(name = "m", required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = " Solde bien modifier",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseCompteDto.class )
+                            )
+                    ),
+                    @ApiResponse(responseCode = "4xx",description = "erreur client"),
+                    @ApiResponse(responseCode = "5xx",description = "erreur serveur"),
+            }
+    )
+
     @PatchMapping("/crediter/{id}/{m}")
     ResponseEntity<ResponseCompteDto> crediter(@PathVariable Integer id, @PathVariable Double m) {
         ResponseCompteDto responseCompteDto = compteService.Crediter(id, m);
         return ResponseEntity.ok(responseCompteDto);
     }
+
+    @Operation(
+            summary = " cette methode pour debiter un compte",
+            parameters = {
+                    @Parameter(name = "id",required = true),
+                    @Parameter(name = "m", required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = " Solde bien modifier",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseCompteDto.class )
+                            )
+                    ),
+                    @ApiResponse(responseCode = "4xx",description = "erreur client"),
+                    @ApiResponse(responseCode = "5xx",description = "erreur serveur"),
+            }
+    )
 
     @PatchMapping("/debiter/{id}/{m}")
     ResponseEntity<ResponseCompteDto> debiter(@PathVariable Integer id, @PathVariable Double m) {
